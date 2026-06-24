54aaf9734174c1e4c9510bc46299f7a68f5f9a6e
zachmoore808-del <zachmoore808@gmail.com>
2026-06-23 20:38:55 -0500

    Landing page at root; full OS preserved at app.html; PWA opens app

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-var CACHE='khaios-v1';
-var SHELL=['./','./index.html','./manifest.json','./icon.svg'];
+var CACHE='khaios-v2';
+var SHELL=['./','./index.html','./app.html','./manifest.json','./icon.svg'];
 self.addEventListener('install',function(e){self.skipWaiting();e.waitUntil(caches.open(CACHE).then(function(c){return c.addAll(SHELL).catch(function(){});}));});
 self.addEventListener('activate',function(e){e.waitUntil(caches.keys().then(function(keys){return Promise.all(keys.map(function(k){if(k!==CACHE)return caches.delete(k);}));}).then(function(){return self.clients.claim();}));});
 self.addEventListener('fetch',function(e){
@@ -8,7 +8,7 @@ self.addEventListener('fetch',function(e){
   var url=new URL(req.url);
   if(url.origin!==self.location.origin)return;
   if(req.mode==='navigate'||(req.headers.get('accept')||'').indexOf('text/html')>-1){
-    e.respondWith(fetch(req).then(function(res){var copy=res.clone();caches.open(CACHE).then(function(c){c.put('./index.html',copy);});return res;}).catch(function(){return caches.match('./index.html').then(function(r){return r||caches.match('./');});}));
+    e.respondWith(fetch(req).then(function(res){var copy=res.clone();caches.open(CACHE).then(function(c){c.put(req,copy);});return res;}).catch(function(){return caches.match(req).then(function(r){return r||caches.match('./index.html').then(function(x){return x||caches.match('./');});});}));
     return;
   }
   e.respondWith(caches.match(req).then(function(cached){return cached||fetch(req).then(function(res){var copy=res.clone();caches.open(CACHE).then(function(c){c.put(req,copy);});return res;}).catch(function(){return cached;});}));
